7abe6b7b8568c5f47017fea7eb7d51a40ee414f4
TammireddiSrinivas <36499709+TammireddiSrinivas@users.noreply.github.com>
2019-05-16 16:15:44 +0530

    Update app.component.ts

--- image-slider/src/app/app.component.ts
+++ image-slider/src/app/app.component.ts
@@ -1,39 +1,54 @@
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+
 @Component({
-  selector: 'my-app',
+
+  selector: 'app-root',
+
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+
+  styleUrls: ['./app.component.css']
+
 })
-export class AppComponent implements OnInit{
-  constructor() { 
-                            
-  }
-  ngOnInit(){
+
+export class AppComponent implements OnInit {
+  title = 'CodeInfo';
+
+  constructor(private _http: HttpClient) {
   }
 
-  imagesData:ImagesJsonFormate[]=[
-    {
-      imageNumber:1,
-      image:"https://tse2.mm.bing.net/th?id=OIP.3HqD3gjGShfdWJ-XowfgagHaE8&pid=Api&w=1200&h=800&rs=1&p=0",
-      captionText:"Nature Image" 
-    },
-    {
-      imageNumber:2,
-      image:"https://tse4.mm.bing.net/th?id=OIP.U-VU51_0yAcu_Fh7x3j2uQHaE8&pid=Api",
-      captionText:"Construction Image" 
-    },
-    {
-      imageNumber:3,
-      image:"https://tse4.mm.bing.net/th?id=OIP.WOo0xJOMl5ZeT1j1qw9IFAHaEo&pid=Api",
-      captionText:"Rainbow Image" 
-    }
-  ]
 
+  imagesData: ImagesJsonFormate[];
+  ngOnInit(): void {
+
+    this.getImages();
+ 
+  }
+
+
+  getImages() {
+    this._http.get('assets/img.json').subscribe((res: any) => {
+ 
+     this.imagesData = res;
+
+    }, error => {
+
+      console.log(error);
+
+    });
+
+  }
+
+
+export interface ImagesJsonFormate {
+
+  imageNumber: number;
+
+  image: string;
+
+  captionText: string;
 
 }
 
-export interface ImagesJsonFormate{
-  imageNumber:number;
-  image:string;
-  captionText:string;
-}
